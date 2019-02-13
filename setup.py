7c45f4e38ec4ad66a1e01e29bc46e186672fac4a
Reinier Schoof <reinier@skoef.nl>
2019-02-13 20:47:28 +0100

    updated setuptools config

--- setup.py
+++ setup.py
@@ -1,31 +1,38 @@
-"""setup.py file."""
-
-import uuid
-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 
 __author__ = 'Reinier Schoof <reinier@skoef.nl>'
 
-install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
-reqs = [str(ir.req) for ir in install_reqs]
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+def parse_reqs(file_path):
+    with open(file_path, 'rt') as fobj:
+        lines = map(str.strip, fobj)
+        lines = filter(None, lines)
+        lines = filter(lambda x: not x.startswith("#"), lines)
+        return tuple(lines)
 
 setup(
     name="napalm-ftos",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     author="Reinier Schoof",
     author_email="reinier@skoef.nl",
-    description="Network Automation and Programmability Abstraction Layer with Multivendor support",
+    description="Network Automation and Programmability Abstraction Layer with Multivendor support, driver for Force10 FTOS",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
     classifiers=[
         'Topic :: Utilities',
          'Programming Language :: Python',
          'Programming Language :: Python :: 2',
          'Programming Language :: Python :: 2.7',
+         'Programming Language :: Python :: 3',
+         'Programming Language :: Python :: 3.7',
         'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
         'Operating System :: MacOS',
     ],
     url="https://github.com/napalm-automation-community/napalm-ftos",
     include_package_data=True,
-    install_requires=reqs,
+    install_requires=parse_reqs('requirements.txt'),
 )
