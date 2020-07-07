c58022d2decd9f903a510ea28ff5fc6c10a46499
Brian Wiborg <baccenfutter@c-base.org>
2020-07-07 11:33:52 +0200

    :wrench: Remove Pyhon2; fully support Python3

--- setup.py
+++ setup.py
@@ -29,9 +29,10 @@ setup(
     classifiers=[
         'Topic :: Utilities',
          'Programming Language :: Python',
-         'Programming Language :: Python :: 2',
-         'Programming Language :: Python :: 2.7',
          'Programming Language :: Python :: 3',
+         'Programming Language :: Python :: 3.4',
+         'Programming Language :: Python :: 3.5',
+         'Programming Language :: Python :: 3.6',
          'Programming Language :: Python :: 3.7',
         'Operating System :: POSIX :: Linux',
         'Operating System :: Microsoft :: Windows',
